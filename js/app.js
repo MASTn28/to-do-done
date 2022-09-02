f6cd750a91fe4e779dc412e51cb7c42e1cd43dfa
Tom Taylor <tomgtaylor@yahoo.com>
2022-09-01 17:19:52 -0700

    Functional constructor, event listener, and DOM manipulation to create a new li when submit is fired.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,50 @@
 'use strict';
 
+// Tom's code under branch tom1
+
+let taskArray = [];
+
+//-------------------- Constructor Function
+
+function Task(text) {
+    this.text = text;
+    taskArray.push(this);
+}
+
+//-------------------- Adds Event Listener with submit and function arguments
+
+let form = document.getElementById("add");
+form.addEventListener('submit', newTask);
+
+//--------------------
+
+function newTask(event) {
+
+    event.preventDefault();    //prevents refresh
+    new Task(event.target.taskName.value);
+    //event.target references something when it's fired, in this case taskName's value.
+    let list = document.getElementById('ul');
+
+    let li = document.createElement('li'); // DOM manipulation
+    li.innerText = event.target.taskName.value;
+    list.append(li);
+    console.log(taskArray);
+}
+
+
 //console.log('On the other side of the screen, it all looks so easy.');
 
 //--------------------GLOBAL VARIABLES/IMPORTS
-let listTitle = '';
-let tasks = []; // array to hold Task objects
+// let listTitle = '';
+// let tasks = []; // array to hold Task objects
 
 
 //--------------------CONSTRUCTORS
-function Task(taskText, taskPriority) {
-    this.text = taskText;
-    this.priority = taskPriority;
-    this.status = '';
-}
+// function Task(taskText, taskPriority) {
+//     this.text = taskText;
+//     this.priority = taskPriority;
+//     this.status = '';
+// }
 
 
 //--------------------CONSTRUCTOR METHODS
@@ -37,8 +69,8 @@ function Task(taskText, taskPriority) {
 
 //--------------------EVENT LISTENERS
 // event listener for create task list submit (aka render task list)
-let taskForm = document.getElementById('taskForm');
-taskForm.addEventListener('submit', createTaskList);
+// let taskForm = document.getElementById('taskForm');
+// taskForm.addEventListener('submit', createTaskList);
 
 // event listener for add additional task
 
@@ -51,9 +83,9 @@ taskForm.addEventListener('submit', createTaskList);
 
 //--------------------EVENT HANDLERS
 // event handler for create task list submit (aka render task list)
-function createTaskList(event) {
-    event.preventDefault();
-    let taskList = event.target;
+// function createTaskList(event) {
+//     event.preventDefault();
+//     let taskList = event.target;
 
 
 
@@ -61,7 +93,6 @@ function createTaskList(event) {
     // function call to render task list on screen
 
     // function call to set task list local storage
-}
 
 // event handler for add new task
 
@@ -74,3 +105,7 @@ function createTaskList(event) {
 
 //--------------------FUNCTION CALLS
 // load page
+
+
+//-------------------- Tom's JS.
+
