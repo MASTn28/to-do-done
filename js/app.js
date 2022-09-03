d3dc7599982aa5e79eecd4c157167d503ed078b7
Angela DeLeon <aedeleon2023@gmail.com>
2022-09-03 13:52:13 -0700

    Worked on CSS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-//-------------------- Global Variables
+// Tom's code under branch tom1
 
-let taskArray = []; // array to hold user's created to-do list tasks
-let listTitle = ''; // user specified name for their to-do list
-let taskCount = 0; // counter for user created to-do list task
+let taskArray = [];
+
+let listTitle = '';
 
 //-------------------- Constructor Function
 
@@ -12,7 +12,6 @@ function Task(text, priority) {
     this.text = text;
     this.priority = priority;
     taskArray.push(this);
-    taskCount++;
 }
 
 //-------------------- Adds Event Listeners with submit and function arguments
@@ -30,12 +29,12 @@ function newTitle(event) {
     getTitle.innerText = listTitle;
 }
 
-//-------------------- Event Listener To Add New Task Item
+//--------------------
 
 let formAdd = document.getElementById('add');
 formAdd.addEventListener('submit', newTask);
 
-//-------------------- Event Handler To Add New Task Item
+//--------------------
 
 function newTask(event) {
 
@@ -45,8 +44,6 @@ function newTask(event) {
     let list = document.getElementById('ul');
 
     let li = document.createElement('li'); // DOM manipulation
-    li.setAttribute('class', 'taskItem');
-    li.setAttribute('id', `task${[taskArray.length - 1]}`);
 
     let objectText = taskArray[taskArray.length - 1].text;
     let objectPriority = taskArray[taskArray.length - 1].priority;
@@ -54,24 +51,11 @@ function newTask(event) {
     list.append(li);
     console.log(taskArray);
 
-    // add Delete button to newly added task item
-    let deleteButton = document.createElement('button');
-    deleteButton.setAttribute('class', 'deleteButton');
-    deleteButton.setAttribute('id', `delete${[taskArray.length - 1]}`);
+    let deleteButton = document.createElement('button1');
     deleteButton.innerHTML = 'Delete';
     li.appendChild(deleteButton);
-
-    // event listener and handler for deleteButton
     deleteButton.addEventListener('click', function() {
         li.remove();
-        console.log(li.id);
-        let index = Number(li.id.substring(4));
-        console.log(index);
-
-        delete taskArray[index];
-        taskArray = taskArray.filter(Boolean);
-
-        console.log(taskArray);
-
+        delete taskArray[taskArray.length - 1];
     });
 }
